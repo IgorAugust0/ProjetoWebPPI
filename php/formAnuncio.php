fdec77966a192230ad040bf522c907098a70d578
Heitor Fillho <heitor.filho@ufu.br>
2023-06-10 21:01:45 -0300

    update no index/produtos

--- php/formAnuncio.php
+++ php/formAnuncio.php
@@ -3,12 +3,12 @@
 session_start();
 
 require "conexaoMysql.php";
-$conn = mysqlConnect();
+$pdo = mysqlConnect();
 
-//$foto = $_FILES["foto"];
+  //$foto = $_FILES["foto"];
 
-//verifica se a foto foi enviada
-if (isset($_POST['titulo'])) {
+  //verifica se a foto foi enviada
+  if(isset($_POST['titulo'])){
   // Resgata os dados do anunciante
   $titulo = $_POST["titulo"] ?? "";
   $categoria = $_POST["categoria"] ?? "";
@@ -22,8 +22,8 @@ if (isset($_POST['titulo'])) {
   $email = $_SESSION['user'];
   $fotos = array();
 
-  if (isset($_FILES['foto'])) {
-    for ($i = 0; $i < count($_FILES['foto']['name']); $i++) {
+  if(isset($_FILES['foto'])){
+    for($i=0; $i < count($_FILES['foto']['name']);$i++){
 
       $nomeArqFoto = $_FILES['foto']['name'][$i];
       move_uploaded_file($_FILES['foto']['tmp_name'][$i], '../images/' . $nomeArqFoto);
@@ -32,9 +32,9 @@ if (isset($_POST['titulo'])) {
       array_push($fotos, $nomeArqFoto);
     }
   }
-} else {
-  echo "Você não realizou o upload de forma satisfatória.";
-}
+}else {
+    echo"Você não realizou o upload de forma satisfatória.";
+  }
 
 
 $sql1 = <<<SQL
@@ -49,49 +49,41 @@ $sql2 = <<<SQL
   VALUES (?,?)
   SQL;
 
-$sql3 = <<<SQL
+  $sql3 = <<<SQL
   SELECT codigo
   FROM anunciante WHERE email = '$email'
   SQL;
 
 
 try {
-  $conn->beginTransaction();
+  $pdo->beginTransaction();
 
-  $sql3 = $conn->query($sql3);
+  $sql3 = $pdo->query($sql3);
   $row = $sql3->fetch();
-  $codAnunciante = $row['codigo'];
+  $codAnunciante= $row['codigo'];
 
-  $stmt1 = $conn->prepare($sql1);
+  $stmt1 = $pdo->prepare($sql1);
   if (!$stmt1->execute([
     $titulo, $descricao, $preco, $dataHora,
     $cep, $estado, $bairro, $cidade, $categoria, $codAnunciante
   ])) throw new Exception('Falha na primeira inserção');
 
-  $codAnuncio = $conn->lastInsertId();
-  $stmt2 = $conn->prepare($sql2);
+  $codAnuncio = $pdo->lastInsertId();
+  $stmt2 = $pdo->prepare($sql2);
   if (!$stmt2->execute([
-    $codAnuncio, $nomeArqFoto
+    $codAnuncio,$nomeArqFoto
   ])) throw new Exception('Falha na segunda inserção');
 
   // Efetiva as operações
-  $conn->commit();
+  $pdo->commit();
 
   header("location: area_anunciante.php");
   exit();
-} catch (Exception $e) {
-  // Caso ocorra algum erro, desfazer a transação
-  $conn->rollBack();
-  $errorCode = $e->getCode();
-  $errorMessage = $e->getMessage();
-  if ($errorCode === 1062) {
-    exit('Dados duplicados: ' . $errorMessage);
-  } else {
-    exit('Falha ao cadastrar os dados: ' . $errorMessage);
-  }
-} finally {
-  // Fechando a conexão com o banco de dados
-  if ($conn !== null) {
-    $conn = null;
-  }
+} 
+catch (Exception $e) {
+  $pdo->rollBack();
+  if ($e->errorInfo[1] === 1062)
+    exit('Dados duplicados: ' . $e->getMessage());
+  else
+    exit('Falha ao cadastrar os dados: ' . $e->getMessage());
 }
